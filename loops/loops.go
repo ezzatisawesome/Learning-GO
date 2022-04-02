353e4a29fa526026a25c1b06aba92461d1ce101f
ezzatisawesome <ezzatisawesome@gmail.com>
2022-04-02 01:01:03 -0500

    nkn kjkj

--- loops/loops.go
+++ loops/loops.go
@@ -3,13 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	/*
-		x := 3
-		for x <= 5 {
-			fmt.Println(x)
-				x++
-		}
-	*/
+
+	x := 3
+	for x <= 5 {
+		fmt.Println(x)
+		x++
+	}
 
 	for x := 0; x <= 5; x++ {
 		fmt.Println(x)
